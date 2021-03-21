70041a545c2d47dd280df05003829a141a1e3203
broncs <jhowfrombrazil@gmail.com>
2021-03-21 14:50:39 -0300

    adding Select component

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
-import Input from './Input';
 import FormikControl from './FormikControl';
 
 const FormikContainer = () => {
+  const dropDownOptions = [
+    {
+      key: 'Select an option',
+      value: '',
+    },
+    {
+      key: 'Option 1',
+      value: 'option1',
+    },
+    {
+      key: 'Option 2',
+      value: 'option2',
+    },
+    {
+      key: 'Option 3',
+      value: 'option3',
+    },
+  ];
   const initialValues = {
     email: '',
     description: '',
+    selectOption: '',
   };
 
   const valitationSchema = Yup.object({
     email: Yup.string().required('Required'),
     description: Yup.string().required('Required'),
+    selectOption: Yup.string().required('Required'),
   });
   const onSubmit = (values) => {
     console.log('console.log', values);
@@ -37,6 +56,12 @@ const FormikContainer = () => {
                 control="textarea"
                 label="Description"
                 name="description"
+              />
+              <FormikControl
+                control="select"
+                label="Select a topic"
+                name="selectOption"
+                options={dropDownOptions}
               />
 
               <button type="submit">Submit</button>
--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -7,7 +7,7 @@ const Textarea = (props) => {
   return (
     <div className="form-control">
       <label htmlFor={name}>{label}</label>
-      <Field as="textarea" name={name} id={name} />
+      <Field as="textarea" name={name} id={name} {...rest} />
       <ErrorMessage name={name} component={TextError} />
     </div>
   );
